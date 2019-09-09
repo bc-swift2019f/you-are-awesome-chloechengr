22e942ba54a741731dc0fef81be12349ccde14ea
chloechengr <chengsn@bc.edu>
2019-09-09 14:47:36 -0400

    2.19 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -32,13 +32,13 @@ class ViewController: UIViewController {
         return newIndex
     }
     
-    func playSound(soundName: String) {
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
         // Can we load in the file soundName?
         if let sound = NSDataAsset(name: soundName) {
             // check if sound.data is a sound file
             do {
-                try awesomePlayer = AVAudioPlayer(data: sound.data)
-                awesomePlayer.play()
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
             } catch {
                 // if sound.data is not a valid audio file
                 print("ERROR: data in \(soundName) counldn't be played as a sound.")
@@ -74,6 +74,6 @@ class ViewController: UIViewController {
         
         // Play a sound
         let soundName = "sound\(soundIndex)"
-        playSound(soundName: soundName)
+        playSound(soundName: soundName, audioPlayer: &awesomePlayer)
     }
 }
